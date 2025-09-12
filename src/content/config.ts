7ba0862e9e76cef52bb512fb697031faef2659a0
Ben Shi <ben@hbish.com>
2025-09-13 00:49:38 +1000

    feat: todo and update frontmatter
    Signed-off-by: Ben Shi <ben@hbish.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,17 +6,49 @@ const postCollection = defineCollection({
         author: z.string(),
         description: z.string().optional(),
         draft: z.boolean().default(false),
-        // Transform string to Date object
+        type: z.literal('post').optional(),
         date: z
             .string()
             .or(z.date())
             .transform((val) => new Date(val)),
-        //heroImage: z.string().optional(),
+        slug: z.string().optional(),
+        tags: z.array(z.string()).optional(),
+        minutesRead: z.string().optional(),
+    }),
+});
+
+const pageCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        description: z.string().optional(),
+        type: z.literal('page').optional(),
+        slug: z.string().optional(),
+        draft: z.boolean().default(false),
+    }),
+});
+
+const noteCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        description: z.string().optional(),
+        type: z.literal('note').optional(),
+        date: z
+            .string()
+            .or(z.date())
+            .transform((val) => new Date(val)),
+        slug: z.string().optional(),
+        categories: z.array(z.string()).optional(),
+        tags: z.array(z.string()).optional(),
+        draft: z.boolean().default(false),
         minutesRead: z.string().optional(),
     }),
 });
 
 export const collections = {
-    'posts': postCollection
+    'posts': postCollection,
+    'pages': pageCollection,
+    'notes': noteCollection,
 };
 
